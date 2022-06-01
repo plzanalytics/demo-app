e12ebb7311204fedfceb388518d13117c70b440a
Jacob <jacobbraslaw@ucsb.edu>
2022-06-01 10:59:42 -0700

    add upload box

--- R/mod_instruction.R
+++ R/mod_instruction.R
@@ -9,7 +9,37 @@
 #' @importFrom shiny NS tagList
 mod_instruction_ui <- function(id){
   ns <- NS(id)
-  tabItem("instruction" )
+  tabItem("instruction",
+  fluidPage(
+    mainPanel(
+      width = 12,
+      h3(strong("Introduction")),
+      tabsetPanel(
+        tabPanel(
+          title = "Instructions",
+          h4(strong("here is instruction panel"), style = "color:green"),
+          h4(strong("1"), "- example bullet style", style = "padding-left:50px")
+          ),
+        tabPanel(
+          title = "Upload File",
+          h4(strong("Upload File using box below"), style = "color:green"),
+          box(
+            title = "Dataset Uploads",
+            width = 8,
+            fluidRow(
+              fileInput(
+                ns("fileInput"),
+                "Choose Excel File",
+                multiple = FALSE,
+                accept = c("xls*")
+                )
+              )
+            )
+          )
+        )
+      )
+    )
+  )
 }
 
 #' instruction Server Functions
