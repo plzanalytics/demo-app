a405b60d28253c978d2c5fc481dbb9f19090c15b
Jacob <jacobbraslaw@ucsb.edu>
2022-06-01 13:19:26 -0700

    create dummy data for UI

--- R/mod_dashboard.R
+++ R/mod_dashboard.R
@@ -33,25 +33,58 @@ mod_dashboard_ui <- function(id){
       tabBox(
         title = "first tab box",
         id = "tab1", height = "300px",
-        tabPanel("tab1.1", "Tabset box 1.1"),
-        tabPanel("tab1.2", "Tabset box 1.2")
+        tabPanel("tab1.1", plotly::plotlyOutput(
+          ns("tab1_1"),
+          height = 230,
+          width = "100%")),
+        tabPanel("tab1.2", plotly::plotlyOutput(
+          ns("tab1_2"),
+          height = 230,
+          width = "100%")),
       ),
       tabBox(
         title = "second tab box",
         id = "tab2", height = "300px",
-        tabPanel("tab2.1", "Tabset box 2.1"),
-        tabPanel("tab2.2", "Tabset box 2.2")
+        tabPanel("tab2.1",  plotly::plotlyOutput(
+          ns("tab2_1"),
+          height = 230,
+          width = "100%")),
+        tabPanel("tab2.2",  plotly::plotlyOutput(
+          ns("tab2_2"),
+          height = 230,
+          width = "100%")),
         )
       ),
       fluidRow(
         shinydashboard::box(
           width = 3, height = 300,
-          title = "param inputs"
+          selectizeInput(
+            ns("employee"),
+            label = "Select employee",
+            choices = c("frank", "heywood", "jablowme", "putjafingainma")
+          ),
+          radioButtons(
+            ns("quarter"),
+            label = "select quarter",
+            choices = c("Q1", "Q2", "Q3", "Q4"),
+            inline = T
+          ),
+          sliderInput(inputId = "UB", label = "Actual Billability %",
+                      min = 0, max = 100, step = 10, value = 50),
+        dateRangeInput(
+          ns("date"),
+          label = "Select Date",
+          start = "1776/07/04",
+          end = Sys.Date()
+        )
         ),
         shinydashboard::box(
           width = 9, height = 300,
-          title = "graph additional"
-        ),
+          title = "Table",
+          DT::dataTableOutput(
+            ns("table")
+          )
+        )
       )
     )
   )
@@ -63,6 +96,26 @@ mod_dashboard_ui <- function(id){
 mod_dashboard_server <- function(id){
   moduleServer( id, function(input, output, session){
     ns <- session$ns
+
+    output$tab1_1 <-  plotly::renderPlotly({
+      shinipsum::random_ggplotly()
+    })
+
+    output$tab1_2 <- plotly::renderPlotly({
+      shinipsum::random_ggplotly()
+    })
+
+    output$tab2_1 <- plotly::renderPlotly({
+      shinipsum::random_ggplotly()
+    })
+
+    output$tab2_2 <- plotly::renderPlotly({
+      shinipsum::random_ggplotly()
+    })
+
+    output$table <- DT::renderDataTable({
+      shinipsum::random_DT()
+    })
 
   })
 }
