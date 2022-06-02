5f77d5cd0c6b3a7081b7d39ce2162f9e5b65034b
Jacob <jacobbraslaw@ucsb.edu>
2022-06-01 20:16:36 -0700

    update dashboard

--- R/mod_dashboard.R
+++ R/mod_dashboard.R
@@ -33,26 +33,33 @@ mod_dashboard_ui <- function(id){
       tabBox(
         title = "first tab box",
         id = "tab1", height = "300px",
-        tabPanel("tab1.1", plotly::plotlyOutput(
-          ns("tab1_1"),
-          height = 230,
-          width = "100%")),
-        tabPanel("tab1.2", plotly::plotlyOutput(
-          ns("tab1_2"),
-          height = 230,
-          width = "100%")),
+        tabPanel("tab1.1"
+          # plotly::plotlyOutput(ns("tab1_1"),
+          # height = 230,
+          # width = "100%")
+          ),
+        tabPanel("tab1.2"
+          # plotly::plotlyOutput(
+          # ns("tab1_2"),
+          # height = 230,
+          # width = "100%")
+          ),
       ),
       tabBox(
         title = "second tab box",
         id = "tab2", height = "300px",
-        tabPanel("tab2.1",  plotly::plotlyOutput(
-          ns("tab2_1"),
-          height = 230,
-          width = "100%")),
-        tabPanel("tab2.2",  plotly::plotlyOutput(
-          ns("tab2_2"),
-          height = 230,
-          width = "100%")),
+        tabPanel("tab2.1"
+          # plotly::plotlyOutput(
+          # ns("tab2_1"),
+          # height = 230,
+          # width = "100%")
+          ),
+        tabPanel("tab2.2"
+          # plotly::plotlyOutput(
+          # ns("tab2_2"),
+          # height = 230,
+          # width = "100%")
+          ),
         )
       ),
       fluidRow(
@@ -76,8 +83,7 @@ mod_dashboard_ui <- function(id){
           label = "Select Date",
           start = "1776/07/04",
           end = Sys.Date()
-        )
-        ),
+        )),
         shinydashboard::box(
           width = 9, height = 300,
           title = "Table"
--- dev/02_dev.R
+++ dev/02_dev.R
@@ -31,16 +31,16 @@ attachment::att_amend_desc()
 
 ## Add modules ----
 ## Create a module infrastructure in R/
-golem::add_module(name = "instruction", with_test = TRUE) # Name of the module
-golem::add_module(name = "dashboard", with_test = TRUE) # Name of the module
-golem::add_module(name = "download", with_test = TRUE) # Name of the module
+golem::add_module(name = "instruction", with_test = F) # Name of the module
+golem::add_module(name = "dashboard", with_test = F) # Name of the module
+golem::add_module(name = "download", with_test = F) # Name of the module
 
 
 
 ## Add helper functions ----
 ## Creates fct_* and utils_*
-golem::add_fct("helpers", with_test = TRUE)
-golem::add_utils("helpers", with_test = TRUE)
+golem::add_fct("helpers", with_test = F)
+golem::add_utils("helpers", with_test = F)
 
 ## External resources
 ## Creates .js and .css files at inst/app/www
